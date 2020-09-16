7a7d6d5aa6b648a0ea571b7ce9df52b03cf2de35
Gleb-afk <71032209+Gleb-afk@users.noreply.github.com>
2020-09-16 21:10:56 +0300

    Update Main.java

--- Laba1/src/com/bsu/Main.java
+++ Laba1/src/com/bsu/Main.java
@@ -10,13 +10,16 @@ public class Main {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Write your k:");
             k = scanner.nextInt();
+            if (k <= 0) {
+                throw new InvalidParameterException("should be more than 0");
+            }
             System.out.println("Let me know your x:");
             x = scanner.nextDouble();
             double eps = Math.pow(10, -k);
-            double actualValue = calculateTaylorSeries(x, eps);
-            double calculatedValue = Math.sinh(x);
-            System.out.println("Actual value: " + formatValue(actualValue));
+            double calculatedValue = calculateTaylorSeries(x, eps);
+            double actualValue = Math.sinh(x);
             System.out.println("Calculated value: " + formatValue(calculatedValue));
+            System.out.println("Actual value: " + formatValue(actualValue));
         } catch (Exception exception) {
             System.out.println(exception);
         }
